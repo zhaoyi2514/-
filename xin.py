780c8b24b89d9134f7a5e32c75508100e4066099
zhaoyi2514 <48511001+zhaoyi2514@users.noreply.github.com>
2019-07-24 20:33:11 +0800

    '输入文字变心'

---
+++ xin.py
@@ -0,0 +1,5 @@
+import time
+words = input('输入你想说的话哦!:')
+for item in words.split():
+    print('\n'.join([''.join([(item[(x-y) % len(item)] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(12, -12, -1)]))
+    time.sleep(1.5)
